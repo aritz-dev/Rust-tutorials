d15fb868dd92b2fb9eb2e196fdca54290fb935ce
aritz-dev <aritz.dev@gmail.com>
2025-08-16 02:02:54 +0200

    guessing game

--- p02_guessing_game/src/main.rs
+++ p02_guessing_game/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::prelude::*;
+//Code that gets a guess from the user and prints it
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    let secret_number:u32=rand::rng().random_range(0..=10);
+    println!("Secret number is: {secret_number}");  
+
+    loop {
+        println!("please input your guess.");
+    
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line!!");
+        //let guess :u32=guess.trim().parse().expect("opss");
+        let guess:u32= match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Mete bien el numero"); 
+                continue;
+            },
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!(" YOU WIN!!");
+                break;// stop loop
+            }
+        }
+    } 
+    
 }
